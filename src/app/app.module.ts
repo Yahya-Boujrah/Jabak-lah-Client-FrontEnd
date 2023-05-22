ce7fd89a200baf14d9feca90fe999f32d8580246
Yassmine-hsaini <yassmina49hsaini@gmail.com>
2023-05-22 16:59:07 +0100

    front-changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FacturesComponent } from './components/factures/factures.component';
+import { CreditorsComponent } from './components/creditors/creditors.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ProduitsComponent } from './components/produits/produits.component';
+import { ProductsComponent } from './components/products/products.component';
 import { HomeComponent } from './components/home/home.component';
 import { AbonnementComponent } from './components/abonnement/abonnement.component';
 import { RechargeComponent } from './components/recharge/recharge.component';
 import { DonationComponent } from './components/donation/donation.component';
+import { BillComponent } from './components/bill/bill.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FacturesComponent,
+    CreditorsComponent,
     NavbarComponent,
-    ProduitsComponent,
+    ProductsComponent,
     HomeComponent,
     AbonnementComponent,
     RechargeComponent,
-    DonationComponent
+    DonationComponent,
+    BillComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/creditors/creditors.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creditors',
+  templateUrl: './creditors.component.html',
+  styleUrls: ['./creditors.component.css']
+})
+export class CreditorsComponent {
+
+}
